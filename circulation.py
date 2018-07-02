1425d983c4608b8c05874c612e3c1de8fd6c0049
CrisMa7 <mayf@cryptape.com>
2018-07-02 20:07:18 +0800

    pracitce for while sentece

---
+++ circulation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#for循环
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+	print(name)
+#while循环
+sum = 0 
+n = 99
+while n > 0:
+	sum = sum +n
+	n -=2
+print(sum)
