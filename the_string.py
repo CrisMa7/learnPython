173d1dc8934c50f92174ca2520c5202e4b130228
CrisMa7 <mayf@cryptape.com>
2018-07-02 15:48:21 +0800

    中文字符编码与解码

--- the_string.py
+++ the_string.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# 求增长的百分比
 s1 = 72
 s2 = 85
 r = (s1-s2)/72*100
 print('%.1f%%'%r)
-
+#中文编码
+s = 'Python-中文'
+print(s)
+b = s.encode('utf-8')
+print(b)
+print(b.decode('utf-8'))
