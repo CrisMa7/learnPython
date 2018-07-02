c515056046526b71220c2f5d59b1dbe731aa2c08
CrisMa7 <mayf@cryptape.com>
2018-07-02 15:28:48 +0800

    pratice the string operation

--- print_absolute_value.py
+++ print_absolute_value.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #print absolute value of an integer:
 origin_value = input('请输入一个整数')
 value = int(origin_value)
---
+++ the_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+s1 = 72
+s2 = 85
+r = (s1-s2)/72*100
+print('%.1f%%'%r)
+
