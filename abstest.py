3687a3b3a2645a3f19163e057ccf0d7c999cc10b
CrisMa7 <mayf@cryptape.com>
2018-07-03 15:55:07 +0800

    practice function

--- abstest.py
+++ abstest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-def my_abs():
+def my_abs(x):
 	if x >=0:
 		return x
 	else:
--- def_func.py
+++ def_func.py
@@ -6,3 +6,16 @@ def move(x, y, step, angle=0):
 	nx = x + step * math.cos(angle)
 	ny = y - step * math.sin(angle)
 	return nx, ny
+
+
+#计算方程ax^2 + bx + c = 0的解 
+def quadratic(a, b, c):
+	if pow(b, 2) - 4*a*c >= 0:
+		temp = math.sqrt(pow(b, 2) - 4*a*c)
+		x1 = (-b + temp)/(2*a)
+		x2 = (-b - temp)/(2*a)
+		return x1, x2
+	else:
+		return "no soulution"
+	
+
